5868c7bc02f8d50a6df349bd8a2f2fe1a09997fc
Emre Ercelebi <emrecercelebi@gmail.com>
2018-02-25 22:33:05 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,53 @@
+var apiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=";
+var apiURLend = "&format=json&callback=?";
+var random = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=2&callback=?";
+
+$(document).ready(function(){
+	$.getJSON(random,function(json){
+		$('#random-link').attr("href","https://en.wikipedia.org/wiki/" + json.query.random[0].title.replace(" ","_"));
+		console.log(json.query.random[0].title);
+	})
+})
+
+function getQuery() {
+	var query = $('#search-bar').val();
+	getWikiData(query);
+}
+
+function getWikiData(query){
+	$.getJSON(apiURL + query + apiURLend, function(json){
+		console.log(json);
+		addResults(json);
+	});
+}
+
+
+function addResults(json) {
+	$('#search-results').html("")
+	if (json[1]){
+		for (let i = 0; i < json[1].length; i++){
+			
+			setTimeout(function(){console.log("adding result");},3000);
+			let resultHTML = "<a href = " + json[3][i] +  " target = '_blank'><div class = 'animated fadeInUp'><div class = 'container card' id = 'result'><h3 id = 'title'>"
+			 + json[1][i] + "</h3><p id ='description'>" + json[2][i] + "</p></div></div></a>";
+			$('#search-results').append(resultHTML);
+		}		
+	}
+}
+
+function setNewRandom(){
+	$.getJSON(random, function(json){
+		$('#random-link').attr("href", "https://en.wikipedia.org/wiki/" + json.query.random[0].title.replace(" ","_"));
+		console.log(json.query.random[0].title);
+	});
+}
+
+
+
+
+
+/*
+<div class = 'container' id = 'result' style='background-color: gray'>
+	<h3 id = 'title'>Result Title</h3>
+	<p id ='description'>Article description will go here</p>				
+</div>*/
